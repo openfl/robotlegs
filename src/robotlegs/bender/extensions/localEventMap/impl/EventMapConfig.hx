6f490b5a02d81072e65307f409a4fe5a069a244a
Kaspars <kasparsj@gmail.com>
2016-02-08 13:13:44 +0200

    EventMapConfig bug fix

--- src/robotlegs/bender/extensions/localEventMap/impl/EventMapConfig.hx
+++ src/robotlegs/bender/extensions/localEventMap/impl/EventMapConfig.hx
@@ -21,65 +21,17 @@ class EventMapConfig
 	/* Public Properties                                                          */
 	/*============================================================================*/
 
-	private var _dispatcher:IEventDispatcher;
 	public var dispatcher(default, null):IEventDispatcher;
-	/**
-	 * @private
-	 */
-	/*public function get_dispatcher():IEventDispatcher
-	{
-		return _dispatcher;
-	}*/
 
-	private var _eventString:String;
 	public var eventString(default, null):String;
-	/**
-	 * @private
-	 */
-	/*public function get_eventString():String
-	{
-		return _eventString;
-	}*/
 
-	private var _listener:Void->Void;
 	public var listener(default, null):Void->Void;
-	/**
-	 * @private
-	 */
-	/*public function get_listener():Void->Void
-	{
-		return _listener;
-	}*/
 
-	private var _eventClass:Class<Dynamic>;
 	public var eventClass(default, null):Class<Dynamic>;
-	/**
-	 * @private
-	 */
-	/*public function get_eventClass():Class<Dynamic>
-	{
-		return _eventClass;
-	}*/
 
-	private var _callback:Dynamic->Void;
 	public var callback(default, null):Dynamic->Void;
-	/**
-	 * @private
-	 */
-	/*public function get_callback():Void->Void
-	{
-		return _callback;
-	}*/
 
-	private var _useCapture:Bool = false;
-	public var useCapture(default, null):Bool;
-	/**
-	 * @private
-	 */
-	/*public function get_useCapture():Bool
-	{
-		return _useCapture;
-	}*/
+	public var useCapture(default, null):Bool = false;
 
 	/*============================================================================*/
 	/* Constructor                                                                */
@@ -90,20 +42,20 @@ class EventMapConfig
 	 */
 	public function new(dispatcher:IEventDispatcher, eventString:String, listener:Void->Void, eventClass:Class<Dynamic>, callback:Dynamic->Void, useCapture:Bool)
 	{
-		_dispatcher = dispatcher;
-		_eventString = eventString;
-		_listener = listener;
-		_eventClass = eventClass;
-		_callback = callback;
-		_useCapture = useCapture;
+		this.dispatcher = dispatcher;
+		this.eventString = eventString;
+		this.listener = listener;
+		this.eventClass = eventClass;
+		this.callback = callback;
+		this.useCapture = useCapture;
 	}
 
 	public function equalTo(dispatcher:IEventDispatcher, eventString:String, listener:Void->Void, eventClass:Class<Dynamic>, useCapture:Bool):Bool
 	{
-		return _eventString == eventString
-			&& _eventClass == eventClass
-			&& _dispatcher == dispatcher
-			&& _listener == listener
-			&& _useCapture == useCapture;
+		return this.eventString == eventString
+			&& this.eventClass == eventClass
+			&& this.dispatcher == dispatcher
+			&& this.listener == listener
+			&& this.useCapture == useCapture;
 	}
 }

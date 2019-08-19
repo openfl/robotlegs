989d849295e7175caf8de1adb5bc5897e64f463a
Thomas Byrne <tom.byrne@imagination.com>
2019-08-19 14:27:56 +1000

    Fixed compilation issue when using msignals

--- src/robotlegs/bender/extensions/signalCommandMap/impl/signals/AppSetupCompleteSignal.hx
+++ src/robotlegs/bender/extensions/signalCommandMap/impl/signals/AppSetupCompleteSignal.hx
@@ -1,12 +1,12 @@
 package robotlegs.bender.extensions.signalCommandMap.impl.signals;
 
-import robotlegs.signal.Signal.Signal0;
+import robotlegs.signal.Signal.Signal0Type;
 
 /**
  * ...
  * @author P.J.Shand
  */
-class AppSetupCompleteSignal extends Signal0 
+class AppSetupCompleteSignal extends Signal0Type 
 {
 	
 	public function new() 

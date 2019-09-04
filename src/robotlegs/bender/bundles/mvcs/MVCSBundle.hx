b629a2400c0c54870164a96d5903b7fb3f7a105c
P.J.Shand <pete@peteshand.net>
2019-09-04 12:40:56 +1000

    Split MVCS and MCS bundles

--- src/robotlegs/bender/bundles/mvcs/MVCSBundle.hx
+++ src/robotlegs/bender/bundles/mvcs/MVCSBundle.hx
@@ -9,26 +9,15 @@ package robotlegs.bender.bundles.mvcs;
 import robotlegs.bender.extensions.contextView.ContextViewExtension;
 import robotlegs.bender.extensions.contextView.StageSyncExtension;
 import robotlegs.bender.extensions.contextView.ContextViewListenerConfig;
-import robotlegs.bender.extensions.directCommandMap.DirectCommandMapExtension;
-import robotlegs.bender.extensions.enhancedLogging.InjectableLoggerExtension;
-// import robotlegs.bender.extensions.enhancedLogging.TraceLoggingExtension;
-import robotlegs.bender.extensions.eventCommandMap.EventCommandMapExtension;
-import robotlegs.bender.extensions.eventDispatcher.EventDispatcherExtension;
-import robotlegs.bender.extensions.localEventMap.LocalEventMapExtension;
-import robotlegs.bender.extensions.logicMap.LogicMapExtension;
-import robotlegs.bender.extensions.modelMap.ModelExtension;
 import robotlegs.bender.extensions.mediatorMap.MediatorMapExtension;
 import robotlegs.bender.extensions.modularity.ModularityExtension;
-import robotlegs.bender.extensions.signalCommandMap.SignalCommandMapExtension;
 import robotlegs.bender.extensions.viewManager.StageCrawlerExtension;
 import robotlegs.bender.extensions.viewManager.StageObserverExtension;
 import robotlegs.bender.extensions.viewManager.ViewManagerExtension;
 import robotlegs.bender.extensions.viewProcessorMap.ViewProcessorMapExtension;
-import robotlegs.bender.extensions.vigilance.VigilanceExtension;
-import robotlegs.bender.extensions.config.ConfigExtension;
+
 import robotlegs.bender.framework.api.IBundle;
 import robotlegs.bender.framework.api.IContext;
-import robotlegs.bender.framework.api.LogLevel;
 
 /**
  * For that Classic Robotlegs flavour
@@ -37,7 +26,7 @@ import robotlegs.bender.framework.api.LogLevel;
  * in typical Robotlegs applications and modules.</p>
  */
 @:keepSub
-class MVCSBundle implements IBundle {
+class MVCSBundle extends MCSBundle implements IBundle {
 	public function new() {}
 
 	/*============================================================================*/
@@ -47,26 +36,16 @@ class MVCSBundle implements IBundle {
 	 * @inheritDoc
 	 */
 	public function extend(context:IContext):Void {
-		context.logLevel = LogLevel.INFO;
+		super.extend(context);
 
-		context.install(InjectableLoggerExtension);
 		context.install(ContextViewExtension);
-		context.install(EventDispatcherExtension);
+		context.install(StageSyncExtension);
 		context.install(ModularityExtension);
-		context.install(DirectCommandMapExtension);
-		context.install(EventCommandMapExtension);
-		context.install(SignalCommandMapExtension);
-		context.install(LocalEventMapExtension);
+		context.install(MediatorMapExtension);
 		context.install(ViewManagerExtension);
 		context.install(StageObserverExtension);
-		context.install(MediatorMapExtension);
-		context.install(LogicMapExtension);
-		context.install(ModelExtension);
 		context.install(ViewProcessorMapExtension);
 		context.install(StageCrawlerExtension);
-		context.install(StageSyncExtension);
-		context.install(ConfigExtension);
-
 		context.configure(ContextViewListenerConfig);
 	}
 }
--- src/robotlegs/signal/Signal.hx
+++ src/robotlegs/signal/Signal.hx
@@ -4,7 +4,7 @@ package robotlegs.signal;
  * This is an abstraction on top of the underlying signals implementation.
  * The purpose is to allow projects to use the signals implementation of their
  * choosing, Robotlegs will then also use this implemtation.
- * 
+ *
  * It can be used by any code that wishes to remain agnostic as to the underlying
  * signal implementation.
  *
@@ -14,95 +14,189 @@ package robotlegs.signal;
  * are guaranteed.
  *
  * The only other supported library currently is 'msignal', others may also be added over time.
- * 
+ *
  * @author Thomas Byrne
  */
-
-
 #if signals
 typedef Signal = signal.Signal;
 typedef Signal1<T> = signal.Signal1<T>;
 typedef Signal2<T, K> = signal.Signal2<T, K>;
-
 typedef SignalType = Signal;
 typedef Signal1Type<T> = Signal1<T>;
 typedef Signal2Type<T, K> = Signal2<T, K>;
-
 #elseif msignal
-
 typedef SignalType = msignal.Signal.Signal0;
 typedef Signal1Type<T> = msignal.Signal.Signal1<T>;
 typedef Signal2Type<T, K> = msignal.Signal.Signal2<T, K>;
 
 @:forward(dispatch, numListeners)
-abstract Signal(msignal.Signal.Signal0) from SignalType
-{
-    public inline function new() this = new msignal.Signal.Signal0();
-    
-    public inline function add(callback:Void->Void, ?fireOnce:Bool=false, ?priority:Int = 0, ?fireOnAdd:Null<Bool> = null) : Void
-    {
-        if(fireOnce){
-            this.addOnceWithPriority(callback, priority);
-        }else{
-            this.addWithPriority(callback, priority);
-        }
-        if(fireOnAdd) callback();
-    }
+abstract Signal(msignal.Signal.Signal0) from SignalType {
+	public inline function new()
+		this = new msignal.Signal.Signal0();
 
-    public function remove(handler:Void->Void=null)
-    {
-        if(handler == null) this.removeAll();
-        else remove(handler);
-    }
+	public inline function add(callback:Void->Void, ?fireOnce:Bool = false, ?priority:Int = 0, ?fireOnAdd:Null<Bool> = null):Void {
+		if (fireOnce) {
+			this.addOnceWithPriority(callback, priority);
+		} else {
+			this.addWithPriority(callback, priority);
+		}
+		if (fireOnAdd)
+			callback();
+	}
+
+	public function remove(handler:Void->Void = null) {
+		if (handler == null)
+			this.removeAll();
+		else
+			remove(handler);
+	}
+
+	/**
+	 * Use the .priority method to specifies the priority the order in which callbacks are fired, higher values will be triggered first.
+	 *
+	 * @param value An optional Int that specifies the priority the order in which callbacks are fired, higher values will be triggered first.
+	 *
+	 * @return BaseSignal<Callback>
+	 */
+	public function priority(value:Int):BaseSignal<Callback> {
+		trace("not implemented");
+		return this;
+	}
+
+	/**
+	 * Use the .repeat method to define the number of times the callback should be triggered before removing itself. Default value = -1 which means it will not remove itself.
+	 *
+	 * @param value An Int that specifies the number of repeats before automatically removing itself.
+	 *
+	 * @return BaseSignal<Callback>
+	 */
+	public function repeat(value:Int = -1):BaseSignal<Callback> {
+		trace("not implemented");
+		return this;
+	}
+
+	/**
+	 * Use the .fireOnAdd method that if called will immediately call the most recently added callback.
+	 *
+	 * @return Void
+	 */
+	public function fireOnAdd():Void {
+		trace("not implemented");
+	}
 }
 
 @:forward(dispatch, numListeners)
-abstract Signal1<T>(msignal.Signal.Signal1<T>) from Signal1Type<T>
-{
-    public inline function new() this = new msignal.Signal.Signal1();
+abstract Signal1<T>(msignal.Signal.Signal1<T>) from Signal1Type<T> {
+	public inline function new()
+		this = new msignal.Signal.Signal1();
 
-    // fireOnAdd=true not yet supported
-    public inline function add(callback:T->Void, ?fireOnce:Bool=false, ?priority:Int = 0) : Void
-    {
-        if(fireOnce){
-            this.addOnceWithPriority(callback, priority);
-        }else{
-            this.addWithPriority(callback, priority);
-        }
-    }
+	// fireOnAdd=true not yet supported
+	public inline function add(callback:T->Void, ?fireOnce:Bool = false, ?priority:Int = 0):Void {
+		if (fireOnce) {
+			this.addOnceWithPriority(callback, priority);
+		} else {
+			this.addWithPriority(callback, priority);
+		}
+	}
 
-    public function remove(handler:T->Void=null)
-    {
-        if(handler == null) this.removeAll();
-        else remove(handler);
-    }
+	public function remove(handler:T->Void = null) {
+		if (handler == null)
+			this.removeAll();
+		else
+			remove(handler);
+	}
+
+	/**
+	 * Use the .priority method to specifies the priority the order in which callbacks are fired, higher values will be triggered first.
+	 *
+	 * @param value An optional Int that specifies the priority the order in which callbacks are fired, higher values will be triggered first.
+	 *
+	 * @return BaseSignal<Callback>
+	 */
+	public function priority(value:Int):BaseSignal<Callback> {
+		trace("not implemented");
+		return this;
+	}
+
+	/**
+	 * Use the .repeat method to define the number of times the callback should be triggered before removing itself. Default value = -1 which means it will not remove itself.
+	 *
+	 * @param value An Int that specifies the number of repeats before automatically removing itself.
+	 *
+	 * @return BaseSignal<Callback>
+	 */
+	public function repeat(value:Int = -1):BaseSignal<Callback> {
+		trace("not implemented");
+		return this;
+	}
+
+	/**
+	 * Use the .fireOnAdd method that if called will immediately call the most recently added callback.
+	 *
+	 * @return Void
+	 */
+	public function fireOnAdd():Void {
+		trace("not implemented");
+	}
 }
 
 @:forward(dispatch, numListeners)
-abstract Signal2<T, K>(msignal.Signal.Signal2<T, K>) from Signal2Type<T, K>
-{
-    public inline function new() this = new msignal.Signal.Signal2();
+abstract Signal2<T, K>(msignal.Signal.Signal2<T, K>) from Signal2Type<T, K> {
+	public inline function new()
+		this = new msignal.Signal.Signal2();
 
-    // fireOnAdd=true not yet supported
-    public inline function add(callback:T->K->Void, ?fireOnce:Bool=false, ?priority:Int = 0, ?fireOnAdd:Null<Bool> = null) : Void
-    {
-        if(fireOnce){
-            this.addOnceWithPriority(callback, priority);
-        }else{
-            this.addWithPriority(callback, priority);
-        }
-        if(fireOnAdd) throw 'Not supported';
-    }
+	// fireOnAdd=true not yet supported
+	public inline function add(callback:T->K->Void, ?fireOnce:Bool = false, ?priority:Int = 0, ?fireOnAdd:Null<Bool> = null):Void {
+		if (fireOnce) {
+			this.addOnceWithPriority(callback, priority);
+		} else {
+			this.addWithPriority(callback, priority);
+		}
+		if (fireOnAdd)
+			throw 'Not supported';
+	}
 
-    public function remove(handler:T->K->Void=null)
-    {
-        if(handler == null) this.removeAll();
-        else remove(handler);
-    }
+	public function remove(handler:T->K->Void = null) {
+		if (handler == null)
+			this.removeAll();
+		else
+			remove(handler);
+	}
+
+	/**
+	 * Use the .priority method to specifies the priority the order in which callbacks are fired, higher values will be triggered first.
+	 *
+	 * @param value An optional Int that specifies the priority the order in which callbacks are fired, higher values will be triggered first.
+	 *
+	 * @return BaseSignal<Callback>
+	 */
+	public function priority(value:Int):BaseSignal<Callback> {
+		trace("not implemented");
+		return this;
+	}
+
+	/**
+	 * Use the .repeat method to define the number of times the callback should be triggered before removing itself. Default value = -1 which means it will not remove itself.
+	 *
+	 * @param value An Int that specifies the number of repeats before automatically removing itself.
+	 *
+	 * @return BaseSignal<Callback>
+	 */
+	public function repeat(value:Int = -1):BaseSignal<Callback> {
+		trace("not implemented");
+		return this;
+	}
+
+	/**
+	 * Use the .fireOnAdd method that if called will immediately call the most recently added callback.
+	 *
+	 * @return Void
+	 */
+	public function fireOnAdd():Void {
+		trace("not implemented");
+	}
 }
-
 #end
-
 
 typedef Signal0 = Signal;
 typedef AnySignal = haxe.extern.EitherType<Signal0, haxe.extern.EitherType<Signal1<Dynamic>, Signal2<Dynamic, Dynamic>>>;
